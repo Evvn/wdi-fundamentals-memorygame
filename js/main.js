2b1474af05d5fee01694cbc6179dbc3a3b20c039
Evan <evvn@outlook.com>
2018-05-31 19:36:54 +1000

    organised game logic with js functions

--- js/main.js
+++ js/main.js
@@ -1,24 +1,33 @@
-console.log("Up and running!");
-
 //array of cards
 var cards = ['queen', 'queen', 'king', 'king'];
+
 //cards in play
 var cardsInPlay = [];
-//first card flipped
-var cardOne =  cards[0];
-//add to cards in play array
-cardsInPlay.push(cardOne);
-//test card flipped in console
-console.log('User flipped ' + cardOne);
-//add second card, third in array
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log('User flipped ' + cardTwo);
-//check if two cards have been played
-if (cardsInPlay.length === 2){
+
+//function to check for match
+var checkForMatch = function () {
   if (cardsInPlay[0] === cardsInPlay[1]) {
     alert('You found a match!');
   } else {
     alert('Sorry, try again.');
   }
-}
+};
+
+//function to store steps when user flips a cardTwo
+var flipCard = function (cardId) {
+
+//display card flipped in console
+console.log('User flipped ' + cards[cardId])
+
+//push selected card in to Play array
+cardsInPlay.push(cards[cardId]);
+
+//Checks if two cards are in play
+  if (cardsInPlay.length === 2){
+    checkForMatch();
+  }
+};
+
+//call flipCard function test
+flipCard(0);
+flipCard(2);

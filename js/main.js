8e82ba7e43d865e4144e8e47260dc94ccd4164d0
Evan <evvn@outlook.com>
2018-05-31 21:24:56 +1000

    created objects for cards

--- js/main.js
+++ js/main.js
@@ -1,5 +1,26 @@
 //array of cards
-var cards = ['queen', 'queen', 'king', 'king'];
+var cards = [
+  {
+    rank: 'queen',
+    suit: 'hearts',
+    cardImage: 'images/queen-of-hearts.png'
+  },
+  {
+    rank: 'queen',
+    suit: 'diamonds',
+    cardImage: 'images/queen-of-diamonds.png'
+  },
+  {
+    rank: 'king',
+    suit: 'hearts',
+    cardImage: 'images/king-of-hearts.png'
+  },
+  {
+    rank: 'king',
+    suit: 'diamonds',
+    cardImage: 'images/king-of-diamonds.png'
+  }
+];
 
 //cards in play
 var cardsInPlay = [];
@@ -13,14 +34,16 @@ var checkForMatch = function () {
   }
 };
 
-//function to store steps when user flips a cardTwo
+//function to store steps when user flips a card
 var flipCard = function (cardId) {
 
 //display card flipped in console
-console.log('User flipped ' + cards[cardId])
+console.log('User flipped ' + cards[cardId].rank);
+console.log(cards[cardId].cardImage);
+console.log(cards[cardId].suit);
 
 //push selected card in to Play array
-cardsInPlay.push(cards[cardId]);
+cardsInPlay.push(cards[cardId].rank);
 
 //Checks if two cards are in play
   if (cardsInPlay.length === 2){

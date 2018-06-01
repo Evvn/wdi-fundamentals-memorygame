4d47abf84c4fa77bd0368688b6fcab34126b9a32
Evan <evvn@outlook.com>
2018-06-01 22:30:48 +1000

    completed game using DOM and added reset icon

--- js/main.js
+++ js/main.js
@@ -21,10 +21,8 @@ var cards = [
     cardImage: 'images/king-of-diamonds.png'
   }
 ];
-
 //cards in play
 var cardsInPlay = [];
-
 //function to check for match
 var checkForMatch = function () {
   if (cardsInPlay[0] === cardsInPlay[1]) {
@@ -33,24 +31,51 @@ var checkForMatch = function () {
     alert('Sorry, try again.');
   }
 };
-
 //function to store steps when user flips a card
-var flipCard = function (cardId) {
-
+var flipCard = function () {
+//store data-id of clicked card in a variable
+var cardId = this.getAttribute('data-id');
 //display card flipped in console
 console.log('User flipped ' + cards[cardId].rank);
 console.log(cards[cardId].cardImage);
 console.log(cards[cardId].suit);
-
 //push selected card in to Play array
 cardsInPlay.push(cards[cardId].rank);
-
+//update src of clicked card to cardImage from cards object
+this.setAttribute('src', cards[cardId].cardImage);
 //Checks if two cards are in play
   if (cardsInPlay.length === 2){
     checkForMatch();
   }
 };
-
-//call flipCard function test
-flipCard(0);
-flipCard(2);
+//function to create game board
+var createBoard = function () {
+  //for loop to loop through the cards array
+  for (var i = 0; i < cards.length; i++) {
+    //create card elements
+    var cardElement = document.createElement('img');
+    cardElement.setAttribute('src', 'images/back.png')
+    cardElement.setAttribute('data-id', i);
+    //give cards id to be able to delete them later
+    cardElement.setAttribute('id', 'card' + i);
+    cardElement.addEventListener('click', flipCard);
+    document.getElementById('game-board').appendChild(cardElement);
+  }
+};
+//call the function to create the board
+createBoard();
+//function to reset the board on reset image clicked
+var resetBoard = function () {
+    //for loop to clear previous cards by card id
+    for (var i = 0; i < cards.length; i++) {
+      var deleteCard = document.getElementById('card' + i);
+      deleteCard.remove();
+      //clear cards in play array
+      cardsInPlay = [];
+    }
+    //call create board function to recreate the board
+    createBoard();
+};
+//add event listener to reset image
+var resetButton = document.getElementById('reset');
+resetButton.addEventListener('click', resetBoard);
